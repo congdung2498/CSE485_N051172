de5d2ade0c54e91bf572fb83c000c29436fb0c35
Lê Công Dũng <congdung2498@gmail.com>
2018-12-03 11:14:09 +0700

    Update layout_foot.php

--- admin/layout_foot.php
+++ admin/layout_foot.php
@@ -2,11 +2,19 @@
     <!-- /container -->
  
 <!-- jQuery library -->
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
- 
+<!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> -->
+
 <!-- Bootstrap JavaScript -->
+ <!-- Dependencies -->
+ <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+  <!-- bootstrap-table -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/bootstrap-table.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/locale/bootstrap-table-en-US.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/extensions/angular/bootstrap-table-angular.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
+<script src="user.js"></script>
 <!-- end the HTML page -->
 </body>
 </html>
