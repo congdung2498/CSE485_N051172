cf47224c1882f24f24d8ebf7ec3d44fc5fe81a65
Lê Công Dũng <congdung2498@gmail.com>
2018-12-04 10:44:56 +0700

    Update layout_foot.php

--- admin/layout_foot.php
+++ admin/layout_foot.php
@@ -13,8 +13,8 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/bootstrap-table.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/locale/bootstrap-table-en-US.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/extensions/angular/bootstrap-table-angular.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="user.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="user.js"></script>
 <!-- end the HTML page -->
 </body>
 </html>
