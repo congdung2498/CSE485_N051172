65b572460d78a44fe98c84c918bfca97d104f2ce
Lê Công Dũng <congdung2498@gmail.com>
2018-12-04 22:45:11 +0700

    Update userControl.php

--- admin/controlPhp/userControl.php
+++ admin/controlPhp/userControl.php
@@ -6,11 +6,14 @@ header("Content-Type: application/json; charset=UTF-8");
 if($_GET['method'] == "load_users")
 {
 	
-	$conn = new mysqli($details['server_host'], $details['mysql_name'],$details['mysql_password'], $details['mysql_database']);	
-	$result = $conn->query("SELECT firstname,lastname,email,contact_number,address,access_level FROM users");
+	$conn = new mysqli($details['server_host'], $details['mysql_name'],$details['mysql_password'], $details['mysql_database']);
+	mysqli_set_charset($conn,"UTF8");	
+	$result = $conn->query("SELECT ID_User,firstname,lastname,email,contact_number,address,access_level FROM users");
+	
 	$data=array();
 	while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
 		$row=array();
+	   $row['ID_User']=addslashes($rs["ID_User"]);
 	   $row['firstname']=addslashes($rs["firstname"]);
        $row['lastname']=addslashes($rs["lastname"]);
        $row['email']=addslashes($rs["email"]);
