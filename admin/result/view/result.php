33ea226592f952d2d01632f9833bf47655efbddc
TrangHoang78 <45060463+TrangHoang78@users.noreply.github.com>
2018-12-10 00:21:03 +0700

    Update result.php

--- admin/result/view/result.php
+++ admin/result/view/result.php
@@ -25,16 +25,7 @@ include '../../layout_head.php';
     </div>
     <table bs-table-control="bsTableResultControl"></table>
     <script src="../controller/result.js"></script>
-<<<<<<< HEAD
-
-</div>
-=======
-</div>
-                    
-
-
->>>>>>> 218ae9acef5477bbbdb5f915c19c3e2867a7b988
- 
+    </div>
   
  <?php
 // include page footer HTML
