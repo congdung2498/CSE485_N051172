b01909e1a798e870aa1235fa0bd7ca6d324683e3
Lê Công Dũng <congdung2498@gmail.com>
2018-12-09 12:31:54 +0700

    Update updateUser.php

--- admin/user/controller/updateUser.php
+++ admin/user/controller/updateUser.php
@@ -5,20 +5,20 @@
 
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
-	$firstname = $request->user->firstname;
-	$lastname = $request->user->lastname;
-	$contact_number = $request->user->contact_number;
-	$access_level = $request->user->access_level;
-	$address = $request->user->address;
-	$email = $request->user->email;
-	$status = $request->user->status;
 
 	$database = new Database();
 	$db = $database->getConnection();
-
 	$user = new User($db);
 
-	$stmt = $user->updateUser($email,$access_level,$status);
+	$user->firstname = $request->user->firstname;
+	$user->lastname = $request->user->lastname;
+	$user->contact_number = $request->user->contact_number;
+	$user->access_level = $request->user->access_level;
+	$user->address = $request->user->address;
+	$user->email = $request->user->email;
+	$user->status = $request->user->status;
+
+	$stmt = $user->updateUser();
 	echo $stmt;
 	
 
