6ae8296c56bfbe52ed01d95fb8bf391e5841d901
TrangHoang78 <45060463+TrangHoang78@users.noreply.github.com>
2018-12-09 22:12:09 +0700

    Create result.js

---
+++ admin/result/controller/result.js
@@ -0,0 +1,80 @@
+var app = angular.module("testApp",  ['bsTable']);
+app.controller("resultCtl", function($scope) {
+    /*$scope.Result=[];
+    $scope.check=false;
+    $scope.check1=false;
+    $scope.result=null;
+    $scope.result={};
+
+    $scope.getResult=function(){
+        $http.get("http://localhost/test-app/admin/result/controller/getResult.php?method=load_result").then(function (response) {
+            console.log(response);
+        $scope.Result = response.data.records;
+        $scope.bsTableResultControl.options.data = $scope.Result;
+        $scope.bsTableResultControl.options.totalRows = $scope.Result.length; 
+    });
+    }
+    $scope.getResult();  */
+    $scope.bsTableResultControl = {
+        options: {
+            /*data: $scope.Result,
+            idField: 'id',
+            sortable: true,
+            striped: true,
+            search: true,
+            maintainSelected: true,
+            clickToSelect: true,
+            showColumns: false,
+            singleSelect: true,
+            showToggle: true,
+            pagination: true,
+            pageSize: 10,
+            pageList: [5, 10, 25, 50, 100],
+            onCheck: function (row, $element) {
+                $scope.$apply(function () {
+                    $scope.check=true;
+                    $scope.result=row;
+                });
+            },
+            onUncheck: function (row, $element) {
+                $scope.check=false;
+               $scope.result={};
+               console.log($scope.check);
+            },*/
+            columns: [{
+                field: 'state',
+                checkbox: true
+            },
+             {
+                field: 'ID_Result',
+                title: 'ID Result',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }, {
+                field: 'ID_User',
+                title: 'ID User',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }
+            , {
+                field: 'ID_Exam',
+                title: 'ID Exam',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }
+            ,
+            {
+                field: 'Score',
+                title: 'Điểm số',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }
+        ]
+        }
+    };
+    
+});
