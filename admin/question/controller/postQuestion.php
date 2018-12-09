ee268a4f4e48472c99fa69de3443be7656d040e6
Lê Công Dũng <congdung2498@gmail.com>
2018-12-09 12:31:22 +0700

    Update postQuestion.php

--- admin/question/controller/postQuestion.php
+++ admin/question/controller/postQuestion.php
@@ -1,34 +1,37 @@
 <?php
 
-    include_once "../../../config/dbconfig.php";
+	include_once "../../../config/database.php";
+	include_once '../../../objects/question.php';
+    
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
 	$contentQs = $request->question->ContentQs;
+	if(isset($request->question->ID_Question)){
+		$ID = $request->question->ID_Question;
+	}
 	$listcontentAs = $request->listanswer;
 	
-	$conn = new mysqli($details['server_host'], $details['mysql_name'],$details['mysql_password'], $details['mysql_database']);	
-    mysqli_set_charset($conn,"UTF8");
-    $result1 = $conn->query("INSERT INTO question (ContentQs) VALUES ('$contentQs')");
-	
-    $result2 = $conn->query("SELECT max(ID_Question) FROM question ");
-	
-    // $qsID = $result2->fetch_array(MYSQLI_ASSOC);
-	$data=array();
-	$rs = $result2->fetch_array(MYSQLI_NUM);
 
-    foreach( $listcontentAs as $as )
-    {
-        $result3 = $conn->query("INSERT INTO answer (ID_Question,ContentAs,Iscorrect) VALUES ($rs[0],'$as->ContentAs','$as->Iscorrect')");
-    }
-    
-	if ($result1 == TRUE && $result2==TRUE && $result3==TRUE ) {
-		echo 1;
-	} else {
-		echo 0 ;
+	$database = new Database();
+	$db = $database->getConnection();
+	$question = new Question($db);
+
+	$question->ContentQs = $contentQs;
+	if(isset($ID)){
+		$question->ID_Question = $ID;
 	}
+	
+	$question->ListAnswer = $listcontentAs;
+	$stmt = $question->createQuestion();
+	echo $stmt;
 	
 
  
  
 
 ?>
+	
+
+ 
+ 
+
