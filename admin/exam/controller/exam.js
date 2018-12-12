ccf0df97152aedaa423dffef82b0a66ca63e5313
Lê Công Dũng <congdung2498@gmail.com>
2018-12-12 13:33:27 +0700

    Update exam.js

--- admin/exam/controller/exam.js
+++ admin/exam/controller/exam.js
@@ -1,9 +1,14 @@
 var app = angular.module("testApp",  ['bsTable']);
+
 app.controller("examCtl", function($scope,$http,$timeout) {
     $scope.Exams=[];
     $scope.check=false;
+    $scope.check2=false;
     $scope.result=null;
     $scope.exam={};
+    $scope.exam.user=[];
+    $scope.Users=[];
+    $scope.selectedUser=[];
 
     $scope.getExams=function(){
         $http.get("http://localhost/test-app/admin/exam/controller/getExam.php?method=load_Exams").then(function (response) {
@@ -16,6 +21,9 @@ app.controller("examCtl", function($scope,$http,$timeout) {
     $scope.getExams();  
     $scope.createExam= function(){
         $scope.exam={};
+        $scope.exam.user=[];
+        $scope.bsTableSelectedUserControl.options.data = [];
+        $scope.bsTableSelectedUserControl.options.totalRows = 0 ;
     }
     $scope.saveExam=function(){
         var request = $http({
@@ -32,6 +40,7 @@ app.controller("examCtl", function($scope,$http,$timeout) {
                 $scope.result=response.data;
                 $scope.getExams();
                 $scope.exam={};
+                $scope.exam.user=[];
                 $timeout($scope.autoHide, 5000);
                 
         });
@@ -59,6 +68,7 @@ app.controller("examCtl", function($scope,$http,$timeout) {
                 $scope.result=response.data;
                 $scope.getExams();
                 $scope.exam={};
+                $scope.exam.user=[];
                 $timeout($scope.autoHide, 5000);
         });
     }
@@ -67,8 +77,41 @@ app.controller("examCtl", function($scope,$http,$timeout) {
             console.log(response);
         $scope.Subjects = response.data.records;
     });
+    } 
+    $scope.getUsers=function(){
+        $http.get("http://localhost/test-app/admin/user/controller/getUsers.php?method=load_users").then(function (response) {
+        $scope.Users = response.data.records;
+        $scope.bsTableUserControl.options.data =  $scope.Users;
+        $scope.bsTableUserControl.options.totalRows = $scope.Users.length; 
+    });
     }
+    $scope.getUsers();  
     $scope.getSubjects();
+    $scope.openUser=function(){
+        $scope.getUsers();  
+    }
+    $scope.addUser= function(){
+        if ($scope.selectedUser.length > 0) {
+            for(var i=0; i<$scope.selectedUser.length;i++){
+                var check=true;
+              for(var j=0; j<$scope.exam.user.length;j++)
+              {
+                  if($scope.selectedUser[i].ID_User==$scope.exam.user[j].ID_User){
+                      check=false;
+                  } 
+              }
+              if(check==true) 
+              {
+                $scope.exam.user.push($scope.selectedUser[i]);
+              }
+            }
+          }
+          $scope.bsTableUserControl.options.data = [];
+          $scope.bsTableUserControl.options.totalRows = 0;
+          $scope.bsTableSelectedUserControl.options.data=$scope.exam.user;
+          $scope.bsTableSelectedUserControl.options.totalRows=$scope.exam.user.length;
+          $scope.selectedUser=[];
+    }
     $scope.autoHide= function(){
         $scope.result=null;
     }
@@ -97,6 +140,7 @@ app.controller("examCtl", function($scope,$http,$timeout) {
             onUncheck: function (row, $element) {
                 $scope.check=false;
                $scope.exam={};
+               $scope.exam.user=[];
             },
             columns: [{
                 field: 'state',
@@ -139,5 +183,126 @@ app.controller("examCtl", function($scope,$http,$timeout) {
         ]
         }
     };
-    
+    $scope.bsTableUserControl = {
+        options: {
+            data: $scope.Users,
+            idField: 'id',
+            sortable: true,
+            striped: true,
+            search:true,
+            maintainSelected: true,
+            clickToSelect: true,
+            showColumns: false,
+            singleSelect: false,
+            showToggle: false,
+            pagination: true,
+            pageSize: 10,
+            pageList: [5, 10, 25, 50, 100],
+            onCheck: function (row, $element) {
+                $scope.$apply(function () {
+                    $scope.selectedUser.push(row);
+                });
+                console.log($scope.selectedUser);
+            },
+            onCheckAll: function (rows) {
+                $scope.selectedUser=[];
+                $scope.$apply(function () {
+                    for(var i=0; i<rows.length; i++){
+                        $scope.selectedUser.push(rows[i]);
+                    }
+                });
+                console.log($scope.selectedUser);
+            },
+            onUncheck: function (row, $element) {
+                var id = row.ID_User;
+               for(var i=0; i<$scope.selectedUser.length; i++){
+                   if(id==$scope.selectedUser[i].ID_User){
+                    $scope.selectedUser.splice(i,1);
+                   }
+               }
+                console.log($scope.selectedUser);
+            },
+            onUncheckAll: function (rows) {
+                $scope.$apply(function () {
+                    $scope.selectedUser.splice($scope.selectedUser.length-rows.length,rows.length);
+                });
+                console.log($scope.selectedUser);
+            },
+            columns: [{
+                field: 'state',
+                checkbox: true
+            },
+             {
+                field: 'firstname',
+                title: 'Họ',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }, {
+                field: 'lastname',
+                title: 'Tên',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }, {
+                field: 'email',
+                title: 'Email',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            }]
+        }
+    };
+
+    $scope.bsTableSelectedUserControl = {
+        options: {
+            data:$scope.exam.user,
+            idField: 'id',
+            sortable: true,
+            striped: true,
+            search:true,
+            maintainSelected: true,
+            clickToSelect: true,
+            showColumns: false,
+            singleSelect: true,
+            showToggle: false,
+            pagination: true,
+            pageSize: 10,
+            pageList: [5, 10, 25, 50, 100],
+            onCheck: function (row, $element) {
+                $scope.$apply(function () {
+                    $scope.check2=true;
+                    $scope.user=row;
+                });
+            },
+            onUncheck: function (row, $element) {
+                $scope.check2=false;
+               $scope.user={};
+               console.log($scope.check);
+            },
+            columns: [{
+                field: 'state',
+                checkbox: true
+            },
+             {
+                field: 'firstname',
+                title: 'Họ',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }, {
+                field: 'lastname',
+                title: 'Tên',
+                align: 'center',
+                valign: 'bottom',
+                sortable: true
+            }, {
+                field: 'email',
+                title: 'Email',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            }]
+        }
+    };
 });
