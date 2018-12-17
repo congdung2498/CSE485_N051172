3ffc1add74b83108e6d44fe74fd8fef75b556ee3
Lê Công Dũng <congdung2498@gmail.com>
2018-12-17 21:42:39 +0700

    Update dashboard.php

--- user/dashboard/view/dashboard.php
+++ user/dashboard/view/dashboard.php
@@ -1,22 +1,22 @@
 <div ng-app="testApp" ng-controller="dashboardCtl">
     <div class="col-md-12">
 
-       <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 exam " ng-repeat="exam in Exams" >
-                <a href="">
+       <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 exam " ng-repeat="examconfig in Exam_Configs" >
+                <a href="user/exam/view/exam.php/{{Exam_Configs.ID_ExamConfig}}">
                     <div class="examName">
-                        {{exam.Name}}
+                        {{examconfig.Name}}
                     </div>
 
                     <div class="examSubjectname">
-                        <span class="glyphicon glyphicon-list-alt"></span> {{exam.subjectName}}
+                        <span class="glyphicon glyphicon-list-alt"></span> {{examconfig.subjectName}}
                     </div>
 
                      <div class="examTotaltime">
-                     <i class="far fa-clock"></i> Thời gian làm bài: {{exam.Totaltime}} phút
+                     <i class="far fa-clock"></i> Thời gian làm bài: {{examconfig.Totaltime}} phút
                     </div>
 
                      <div class="examNumberQs">
-                     <i class="fas fa-list-ol"></i> Số lượng câu hỏi: {{exam.Num_Question}} câu
+                     <i class="fas fa-list-ol"></i> Số lượng câu hỏi: {{examconfig.Num_Question}} câu
                     </div>
 
                  </a>
