56c26aeb9714d341c0a12c3df73b914b41f9873c
Lê Công Dũng <congdung2498@gmail.com>
2018-12-17 21:41:53 +0700

    Update exam.php

--- objects/exam.php
+++ objects/exam.php
@@ -9,11 +9,9 @@ class Exam
 	private $table_name = "exam";
 
 	public $ID_Exam;
-    public $Name;
-    public $Num_Question;
-    public $Totaltime;
-	public $ID_Subject;
-	public $ListUser;
+	public $ID_ExamConfig;
+	public $ID_User;
+	public $score;
 	public function __construct($db)
 	{
 		$this->conn = $db;
@@ -21,96 +19,45 @@ class Exam
 
 	public function createExam(){
 
-		if(!isset($this->ID_Exam)){  //them moi
-			$query = "INSERT INTO ".$this->table_name."
-		SET Name = :Name, Num_Question = :Num_Question, Totaltime = :Totaltime, ID_Subject = :ID_Subject";
+		$query = "INSERT INTO ".$this->table_name."
+		SET
+		ID_ExamConfig = :ID_ExamConfig,
+		ID_User= :ID_User";
 		
 		$stmt = $this->conn->prepare($query);
-		$stmt->bindParam(':Name', $this->Name);
-		$stmt->bindParam(':Num_Question', $this->Num_Question);
-		$stmt->bindParam(':Totaltime', $this->Totaltime);
-		$stmt->bindParam(':ID_Subject', $this->ID_Subject);
+		$stmt->bindParam(':ID_ExamConfig', $this->ID_ExamConfig);
+		$stmt->bindParam(':ID_User', $this->ID_User);
 		if($stmt->execute()) $rs=1;
 		else $rs=0;
+					
 		$IDExam=$this->getIDExam();
-			foreach( $this->ListUser as $us ) 
-			{
-			$query2="INSERT INTO exam_user SET ID_User = :ID_User, ID_Exam = :ID_Exam";
-			$stmt2 = $this->conn->prepare($query2);
-			
-			$stmt2->bindParam(':ID_User',$us->ID_User);
-			$stmt2->bindParam(':ID_Exam', $IDExam);
-			$rs2 = $stmt2->execute() ;
-			}	
 
 
-		if ($rs == 1 && $rs2==true) {
+		$query2 = "SELECT ID_Question FROM question a , exam_config b Where a.ID_Subject = b.ID_Subject and b.ID_ExamConfig =10";
+    	$stmt = $this->conn->prepare( $query2 );
+    	$stmt->execute();
+		$data=array();
+		while ($rs = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		   $row=array();
+		   $row['ID_Question']=(int)$rs["ID_Question"];
+		   $data[]=$row;
+		}
+	
+		
+		$Number_Question=$this->getNum_Question();
+
+		for($i =0; $i < count($data); $i++)
+		echo $data[$i]['ID_Question'];
+
+		
+
+		if ($rs == 1) {
 			echo 1;
 		}else{
 			echo 0;
         }
         
-		}
-
-		else{ //sua
-			$query = "UPDATE ".$this->table_name." SET Name = :Name, Num_Question = :Num_Question, Totaltime = :Totaltime, ID_Subject = :ID_Subject WHERE ID_exam = ".$this->ID_Exam;
-			$stmt = $this->conn->prepare($query);
-			$stmt->bindParam(':Name', $this->Name);
-			$stmt->bindParam(':Num_Question', $this->Num_Question);
-			$stmt->bindParam(':Totaltime', $this->Totaltime);
-			$stmt->bindParam(':ID_Subject', $this->ID_Subject);
-			if($stmt->execute()) $rs=1;
-			else $rs=0;     
-
-			$query = "DELETE from exam_user Where ID_Exam = ".$this->ID_Exam; //xoa user 
-			$stmt = $this->conn->prepare($query);
-			if($stmt->execute()) $rs2=1;
-			else $rs2=0;     
-
-			foreach( $this->ListUser as $us ) //them lai user
-			{
-			$query2="INSERT INTO exam_user SET ID_User = :ID_User, ID_Exam = :ID_Exam";
-			$stmt2 = $this->conn->prepare($query2);
-			
-			$stmt2->bindParam(':ID_User',$us->ID_User);
-			$stmt2->bindParam(':ID_Exam', $this->ID_Exam);
-			$rs2 = $stmt2->execute() ;
-			}	
-
-				if ($rs == 1 && $rs2 ==true) {
-					echo 1;
-				}else{
-					echo 0;
-				}
-		}
 		
-	}
-
-    public function getExams(){
-    	$query = "SELECT ID_Exam , Name, Num_Question, Totaltime,subjectName FROM exam a, subject b where a.ID_Subject= b.ID_Subject ";
-    	$stmt = $this->conn->prepare( $query );
-    	$stmt->execute();
-    	return $stmt;
-	}
-
-	public function deleleExam(){   
-		$query3 = "DELETE FROM exam_user WHERE ID_Exam=".$this->ID_Exam; 
-    	$stmt3 = $this->conn->prepare( $query3 );
-    	if($stmt3->execute()) $rs3=1;
-		else $rs3=0;
-
-		$query2 = "DELETE FROM exam_question WHERE ID_Exam=".$this->ID_Exam; 
-    	$stmt2 = $this->conn->prepare( $query2 );
-    	if($stmt2->execute()) $rs2=1;
-		else $rs2=0;
-
-		$query1 = "DELETE FROM exam WHERE ID_Exam=".$this->ID_Exam; 
-    	$stmt1 = $this->conn->prepare( $query1 );
-    	if($stmt1->execute()) $rs1=1;
-		else $rs1=0;
-
-		if($rs1==1 && $rs2==1 && $rs3==1) echo 1;
-		else echo 0;
 	}
 	public function getIDExam(){
     	$query = "SELECT max(ID_Exam) FROM exam";
@@ -119,12 +66,25 @@ class Exam
 		$rs = $stmt->fetch(PDO::FETCH_NUM);
 		return $rs[0];
 	}
-	public function getUserbyExamID(){
-		$query = "SELECT a.ID_User,firstname,lastname,email FROM users a , exam_user b WHERE a.ID_User = b.ID_User and b.ID_Exam =".$this->ID_Exam;
+
+    public function getNum_Question(){
+    	$query = "SELECT Num_Question  FROM exam_config a, exam b WHERE a.ID_ExamConfig = b.ID_ExamConfig and b.ID_Exam=".$this->getIDExam();
     	$stmt = $this->conn->prepare( $query );
     	$stmt->execute();
-    	return $stmt;
+    	$rs = $stmt->fetch(PDO::FETCH_NUM);
+		return $rs[0];
 	}
+
+	public function deleleSubject(){   
+		$query1 = "DELETE FROM subject WHERE ID_Subject=".$this->ID_Subject; 
+    	$stmt1 = $this->conn->prepare( $query1 );
+    	if($stmt1->execute()) $rs1=1;
+		else $rs1=0;
+
+		if($rs1==1) echo 1;
+		else echo 0;
+	}
+
 }
 
  ?>
