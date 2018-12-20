209e28a683d4a09d3126656aff1dfcbc27460dfb
CongDung <congdung2498@gmail.com>
2018-12-20 23:00:43 +0700

    Update dashboard.js

--- N051172/Sources/user/dashboard/controller/dashboard.js
+++ N051172/Sources/user/dashboard/controller/dashboard.js
@@ -2,7 +2,7 @@ var app = angular.module("testApp",  ['bsTable']);
 // var id=1;
 app.controller("dashboardCtl", function($scope,$http,$timeout,$rootScope,$location) {
     $scope.getExamConfigs=function(){
-        $http.get("http://localhost:81/test-app/user/dashboard/controller/getExamConfigs.php?method=load_ExamConfigs").then(function (response) {
+        $http.get("http://localhost/CSE485_N051172/N051172/Sources/user/dashboard/controller/getExamConfigs.php?method=load_ExamConfigs").then(function (response) {
         $scope.Exam_Configs = response.data.records;
         console.log($scope.Exam_Configs);
     });
