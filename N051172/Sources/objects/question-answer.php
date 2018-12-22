c5bdb0a74977d2db435e200ee7597d35fdb9c1f2
CongDung <congdung2498@gmail.com>
2018-12-23 00:10:56 +0700

    Update question-answer.php

--- N051172/Sources/objects/question-answer.php
+++ N051172/Sources/objects/question-answer.php
@@ -44,8 +44,24 @@ class QuestionAnswers
 		}
 		array_push($data,$question);
 		}
+
+		$query = "SELECT a.ID_ExamConfig , Name, Num_Question, Totaltime,subjectName FROM exam_config a, subject b, exam c where a.ID_Subject= b.ID_Subject and a.ID_ExamConfig=c.ID_ExamConfig and c.ID_Exam = ".$ExID;
+    	$stmt = $this->conn->prepare( $query );
+    	$stmt->execute();   //lay thong tin de thi
+    	$config=array();
+		while ($rs = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		   $row=array();
+		   $row['ID_ExamConfig']=(int)$rs["ID_ExamConfig"];
+		   $row['Name']=addslashes($rs["Name"]);
+		   $row['Num_Question']=(int)$rs["Num_Question"];
+		   $row['Totaltime']=(int)$rs["Totaltime"];
+		   $row['subjectName']=addslashes($rs["subjectName"]);
+		   $config[]=$row;
+		}
+
 		$jsonData=array();
 		$jsonData['records']=$data;
+		$jsonData['config']=$config;
 		echo json_encode($jsonData);
 
 	}
