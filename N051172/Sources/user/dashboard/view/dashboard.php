60670c56d67a889d5ba35a06c2f5b7a44d68ad5e
CongDung <congdung2498@gmail.com>
2018-12-22 21:36:30 +0700

    Update dashboard.php

--- N051172/Sources/user/dashboard/view/dashboard.php
+++ N051172/Sources/user/dashboard/view/dashboard.php
@@ -2,7 +2,8 @@
     <div class="col-md-12">
 
        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 exam " ng-repeat="examconfig in Exam_Configs" >
-                <a href="user/exam/view/exam.php/{{examconfig.ID_ExamConfig}}">
+                <a href="#" ng-click="open(examconfig)" data-toggle="modal" data-target="#myModal">
+                <!-- user/exam/view/exam.php/{{examconfig.ID_ExamConfig}} -->
                     <div class="examName">
                         {{examconfig.Name}}
                     </div>
@@ -21,6 +22,51 @@
 
                  </a>
         </div>
+
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header" id="mydivheader">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title"> Thông tin</h4>
+                    </div>
+                    <div class="modal-body">
+
+                     <div class="panel panel-default" style="border: 0px ">
+                         <div class="panel-body" style="font-size: 18px;">
+
+                        <div class="row">
+                                <label class="col-xs-12 bold">Tên đề thi: {{Config.Name}}</label>
+                        </div>    
+                        
+                        <div class="row">
+                                <label class="col-xs-12 bold">Tên môn học: {{Config.subjectName}}</label>
+                        </div> 
+                         
+                        <div class="row">
+                                <label class="col-xs-12 bold">Số lượng câu hỏi: {{Config.Num_Question}}</label>
+                        </div>
+
+                        <div class="row">
+                                <label class="col-xs-12 bold">Thời gian làm bài: {{Config.Totaltime}} phút</label>
+                        </div>
+                         
+                         </div>                         
+
+
+                    </div>
+
+
+                    <div class="modal-footer">
+                        <button type="button" ng-click= <?php echo isset($_SESSION['user_id']) ? "check($_SESSION[user_id],Config.ID_ExamConfig)" : "login()"; ?> class="btn btn-success" data-dismiss="modal">Bắt đầu làm bài</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 
     <script src="user/dashboard/controller/dashboard.js"></script>
