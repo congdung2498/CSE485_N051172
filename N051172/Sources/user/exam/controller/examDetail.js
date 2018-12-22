c488981965b7b2d1a1a56a567ff4590d7e8456bc
CongDung <congdung2498@gmail.com>
2018-12-22 21:37:25 +0700

    Update examDetail.js

--- N051172/Sources/user/exam/controller/examDetail.js
+++ N051172/Sources/user/exam/controller/examDetail.js
@@ -2,40 +2,34 @@ var app = angular.module("testApp",  ['bsTable']);
 
 app.controller("ExamDetailCtl", function($scope,$http,$timeout,$location) {
     
-    $scope.getExams=function(){
-        $http.get("http://localhost/CSE485_N051172/N051172/Sources/user/exam/controller/getExam.php?method=load_Exams").then(function (response) {
-        $scope.Exams = response.data.records;
-        console.log($scope.Exams);
-    });
-    }
-    $scope.getExams();
-    // $scope.session = {id: $stateParams.ID_Exam || null};
-    // alert($scope.session);
+   
     var absUrl = $location.absUrl();
     var a=absUrl.lastIndexOf("/");
     var s="";
     for(var i=a+1; i < absUrl.length; i++ ){
         s=s+""+absUrl[i];
     }
-    $scope.ID= parseInt(s);
-    $scope.RandomQS= function(){
+    $scope.examID= parseInt(s);
+    console.log($scope.examID);
+    $scope.getQuestions= function(){
             var request = $http({
                 method: "POST",
-                url: "http://localhost/CSE485_N051172/N051172/Sources/admin/exam/controller/postExam.php?method=post_question",
+                url: "http://localhost/CSE485_N051172/N051172/Sources/user/exam/controller/getQuestions.php?method=load_Ques",
                 data: {
-                    exam: $scope.exam
+                    examID:  $scope.examID
                 },
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                 });
             
             /* Check whether the HTTP Request is successful or not. */
                 request.then(function (response) {
-                    $scope.result=response.data;
-                    $scope.getExams();
-                    $scope.exam={};
-                    $scope.exam.user=[];
-                    $timeout($scope.autoHide, 5000);
-                    
+                    $scope.Questions=response.data.records;
+                    for(var i=0; i<$scope.Questions.length; i++){
+                        $scope.Questions[i].index=i+1;
+                    }
+                    console.log( $scope.Questions);
             });
     }
+    $scope.getQuestions();
+    
 });
