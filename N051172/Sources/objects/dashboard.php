26a783c33cd8e64af976071d79a9423420f9f083
CongDung <congdung2498@gmail.com>
2018-12-22 21:35:01 +0700

    Update dashboard.php

--- N051172/Sources/objects/dashboard.php
+++ N051172/Sources/objects/dashboard.php
@@ -13,6 +13,7 @@ class ExamConfig
     public $Num_Question;
     public $Totaltime;
 	public $SubjectName;
+	public $UserID;
 	public function __construct($db)
 	{
 		$this->conn = $db;
@@ -26,6 +27,23 @@ class ExamConfig
     	return $stmt;
 	}
 
+	public function check(){
+		$query="select count(ID_Exam) from exam where ID_User=".$this->UserID." and ID_ExamConfig=".$this->ID_ExamConfig;
+		$stmt = $this->conn->prepare( $query );
+		$stmt->execute();
+		$rs = $stmt->fetch(PDO::FETCH_NUM);
+		$number= $rs[0];
+		if($number>=1) return '1'; // đã làm đề
+		else{
+			$query2="select count(ID_ex_us) from exam_config_user where ID_User=".$this->UserID." and ID_ExamConfig=".$this->ID_ExamConfig;
+			$stmt2 = $this->conn->prepare( $query2);
+			$stmt2->execute();
+			$rs2 = $stmt2->fetch(PDO::FETCH_NUM);
+			$number2= $rs2[0];
+			if($number2 ==0) return '2'; //không có trong danh sách thi 
+			else return '0'; //đủ điều kiện thi  
+		}
+	}
 	
 }
 
