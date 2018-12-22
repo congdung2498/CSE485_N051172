459c8172f5dc33a5e3a434f9662c6d689fc94aae
CongDung <congdung2498@gmail.com>
2018-12-22 21:34:41 +0700

    Update layout_foot.php

--- N051172/Sources/layout_foot.php
+++ N051172/Sources/layout_foot.php
@@ -1,7 +1,8 @@
 </div>
     <!-- /container -->
- 
+<div class= "footer" class="page-wrapper-row" style="width:100% ;height:100px;background:#3b434c;" >
 
+</div>
 <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> -->
  
 <!-- Bootstrap JavaScript -->
