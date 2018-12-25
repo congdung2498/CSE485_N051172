da28e6c896564cff43617814bc6f79bb91ca9291
CongDung <congdung2498@gmail.com>
2018-12-25 17:51:13 +0700

    Update examDetail.js

--- N051172/Sources/user/exam/controller/examDetail.js
+++ N051172/Sources/user/exam/controller/examDetail.js
@@ -1,8 +1,11 @@
 var app = angular.module("testApp",  ['bsTable']);
 
-app.controller("ExamDetailCtl", function($scope,$http,$timeout,$location) {
+app.controller("ExamDetailCtl", function($scope,$http,$timeout,$location,$window) {
     
     var time;
+    $scope.Questions=[];
+    $scope.SubmitList=[];
+    $scope.Isfinish=false;
     var absUrl = $location.absUrl();
     var a=absUrl.lastIndexOf("/");
     var s="";
@@ -42,13 +45,73 @@ app.controller("ExamDetailCtl", function($scope,$http,$timeout,$location) {
                 countdown: true,
                 callbacks: {
                     stop: function() {
-                        $('.message').html('The clock has stopped!');
+                        $scope.Submit ($scope.Questions);
                     }
                 }
             });
 
         });
     }
+
+    $scope.checkoptions = function (question) {
+        // var details = [];
+        // angular.forEach(choice, function (value, key) {
+        // if (choice[key].checked) {
+        // details.push(choice[key].userid);
+        // }
+        // });
+        // console.log(details);
+    $scope.numberAnswer=0;
+        for(var i=0 ; i<question.length;i++){
+            var check=false;
+            for(var j=0; j<question[i].ListAnswer.length; j++){
+                if(question[i].ListAnswer[j].checked!=null && question[i].ListAnswer[j].checked==true){
+                    check=true;
+                }
+            }
+            if(check==true) $scope.numberAnswer=$scope.numberAnswer+1;
+        }
+        console.log($scope.numberAnswer);
+
+        };
+
+        $scope.Submit = function (question) {
+
+            for(var i=0 ; i<question.length;i++){
+                for(var j=0; j<question[i].ListAnswer.length; j++){
+                    if(question[i].ListAnswer[j].checked!=null && question[i].ListAnswer[j].checked==true){
+                        $scope.SubmitList.push(question[i].ListAnswer[j]);
+                    }
+                }
+            }
+
+            var request = $http({
+                method: "POST",
+                url: "http://localhost/CSE485_N051172/N051172/Sources/user/exam/controller/submit.php?method=submit",
+                data: {
+                    ListAnswer:  $scope.SubmitList,
+                    examID:  $scope.examID
+
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                });
+            
+            /* Check whether the HTTP Request is successful or not. */
+                request.then(function (response) {
+                    $scope.checkFinish=response.data.check;
+                    if($scope.checkFinish ==0){
+                        alert("Bạn đã làm xong bài thi. Hệ thống sẽ quay lại trang chủ.");
+                        $window.location.href="http://localhost/CSE485_N051172/N051172/Sources"
+                    }
+                    if(response.data.numberCorrect!=null && response.data.score !=null){
+                        $scope.NumberCorecct=response.data.numberCorrect;
+                        $scope.Score=response.data.score;
+                        $scope.Isfinish=true;
+                        $scope.timeclock(0);
+                    }
+                   
+            });
+            };
    
     
 });
