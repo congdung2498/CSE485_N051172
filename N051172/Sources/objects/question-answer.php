50d1486ca85d0a9388a3deddc047f36028167edc
CongDung <congdung2498@gmail.com>
2018-12-25 17:49:48 +0700

    Update question-answer.php

--- N051172/Sources/objects/question-answer.php
+++ N051172/Sources/objects/question-answer.php
@@ -30,7 +30,7 @@ class QuestionAnswers
 		    
 
 		   
-		$query2 = "SELECT ID_Answer, ContentAs , Iscorrect FROM answer a , question b , exam_question c
+		$query2 = "SELECT ID_Answer,a.ID_Question, ContentAs , Iscorrect FROM answer a , question b , exam_question c
 		Where a.ID_Question = b.ID_Question and b.ID_Question= c.ID_Question and b.ID_Question =". $question->ID_Question." and c.ID_Exam =".$ExID;	
     	$stmt2 = $this->conn->prepare( $query2);
 		$stmt2->execute();
@@ -38,6 +38,7 @@ class QuestionAnswers
 		while ($rs = $stmt2->fetch(PDO::FETCH_ASSOC)) {
            $answer= new Answer();
 		   $answer->ID_Answer=(int)$rs["ID_Answer"];
+		   $answer->ID_Question=(int)$rs["ID_Question"];
 		   $answer->ContentAs=$rs["ContentAs"];
 		   $answer->Iscorrect=(int)$rs["Iscorrect"];		   
 		   array_push($question->ListAnswer,$answer);
